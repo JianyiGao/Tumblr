af187b24f5e6017a902d9f1741c4e983db905a56
Jianyi Gao <gaojianyi912@gmail.com>
2017-01-07 18:16:08 -0500

    Initial Commit

---
+++ Tumblr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tumblr
+//
+//  Created by Jianyi Gao 高健一 on 1/7/17.
+//  Copyright © 2017 Jianyi Gao 高健一. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
